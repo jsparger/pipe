01047d71a391fd82c6a2935dfd7f1c487fe72b5f
John Sparger <jsparger87@gmail.com>
2014-09-09 18:44:30 -0400

    added doxygen to BundleAccess

--- include/pipe/BundleAccess.hh
+++ include/pipe/BundleAccess.hh
@@ -17,14 +17,21 @@
 
 namespace pipe {
 
+//TODO: What about move semantics? I thought I fixed this?
+
+/// \class BundleAccess
+/// \brief Used to write/read data from a MessageBundle with naming determined by message type.
+///
+/// BundleAccess will allow you to write/read data of type \p T to/from a MessageBundle. BundleAccess relies on a static method T::GetMessageType() to name the data. This allows you to retrieve data based on the type of data. However, this means there can only be one instance of a given data type in the map. To retrieve abritrarily named data from the MessageBundle, use BundleAccessByName.
 template <class T> 
 class BundleAccess : public MessageBundle::Accessor
 {
 public:
+	
+	/// Destructor
 	virtual ~BundleAccess() {;}
 
-	// get the message of type T from the bundle if it exists (check first with inspect).
-	// will throw error if message does not exist.
+	/// Get the message of type T from the bundle if it exists (check first with inspect). Will return false if message does not exist. Will throw exception if unpacking type erased data fails.
 	virtual bool readFrom(std::unique_ptr<MessageBundle>& bundle, T& message)
 	{
 		// get the map from the bundle
@@ -39,7 +46,7 @@ public:
 		return true;
 	}
 	
-	// I just don't like this. It seems dangerous. Should probably delete this method.
+	/// I just don't like this. It seems dangerous. Should probably delete this method.
 	virtual const T& readRef(std::unique_ptr<MessageBundle>& bundle)
 	{
 		// get the map from the bundle
@@ -56,8 +63,7 @@ public:
 		return *boost::any_cast<T>(&wrapped);
 	}
 	
-	// attach the message of type T to the bundle. Returns false if message of type
-	// T has already been attached to bundle
+	/// Attach the message of type T to the bundle. Returns false if message of type T has already been attached to bundle
 	virtual bool attachTo(std::unique_ptr<MessageBundle>& bundle, const T& message)
 	{
 			// get the map from the bundle
@@ -72,6 +78,7 @@ public:
 			return true;
 	}
 	
+	/// Check to see if a message of type \p T exists in the bundle.
 	virtual bool hasMessage(std::unique_ptr<MessageBundle>& bundle)
 	{
 		// get the map from the bundle
@@ -80,13 +87,18 @@ public:
 	}
 };
 	
-
+/// \class BundleAccessByName
+/// \brief Used to write/read data from a MessageBundle with arbitrary naming.
+///
+/// BundleAccess will allow you to write/read data of type \p T to/from a MessageBundle using an arbitrary name.
 template <class T> 
 class BundleAccessByName : public MessageBundle::Accessor
 {
 public:
+	/// Destructor
 	virtual ~BundleAccessByName() {;}	
 	
+	/// check to see if data associated with the string \p name exists in the bundle.
 	virtual bool checkFor(std::unique_ptr<MessageBundle>& bundle, const std::string& name)
 	{
 		auto& map = getMap(*bundle);
@@ -94,16 +106,19 @@ public:
 		return (map.end() != it);
 	}
 	
+	/// Retrieve a constant reference to the data associated with the string \p name from the bundle. Will throw an error if the string is not associated with any message or if unpacking the data to type \p T fails.
 	virtual const T& readRef(std::unique_ptr<MessageBundle>& bundle, const std::string& name)
 	{
 		auto& map = getMap(*bundle);
 		auto it = map.find(name);
+		// TODO: throw a real error.
 		if (map.end() == it) { std::cerr << "BundleAccess::readRef(...): Error! No message of requested type!"; std::exit(0); }
 		boost::any& theAny = it->second;
 		T* ret = boost::any_cast<T>(&theAny);
 		return *ret;
 	}
 	
+	/// Attach the data in \p message to the bundle and associate it with the string \name.
 	virtual bool attachTo(std::unique_ptr<MessageBundle>& bundle, const T& message, const std::string& name)
 	{
 		// get the map from the bundle
